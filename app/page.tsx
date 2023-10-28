74eb3e78577b4d9bdd3f95e8992088430be4bb8a
r-kojima <kojimar24@gmail.com>
2023-10-28 20:45:47 +0900

    optimizing images

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { lusitana } from "@/app/ui/fonts";
 import Link from "next/link";
+import Image from "next/image";
+
 export default function Page() {
   const test = "";
   return (
@@ -28,7 +30,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={620}
+            height={560}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
         </div>
       </div>
     </main>
